3680133d1553762d0ba90bc0620f8359b7dac7f9
Mayuri <102599821+MMContis@users.noreply.github.com>
2023-06-19 17:04:33 +0530

    IDENT-8388 | Add two "Localizable.strings" for the same language in the same module (#379)
    * Added second localise files of same language in all module

--- Modules/Core/IdentHubSDKCore/Extensions/Framework/String+Localization.swift
+++ Modules/Core/IdentHubSDKCore/Extensions/Framework/String+Localization.swift
@@ -20,9 +20,17 @@ extension String {
     func localized(fromBundle bundle: Bundle, forLanguageCode languageCode: String) -> String {
         guard let path = bundle.path(forResource: languageCode, ofType: "lproj"),
               let localizationBundle = Bundle(path: path) else {
-            return bundle.localizedString(forKey: self, value: self, table: nil)
+                var localized = NSLocalizedString(self, tableName: "InitialString", bundle: bundle, value: self, comment: "")
+                if localized == self {
+                    localized = bundle.localizedString(forKey: self, value: self, table: nil)
+                }
+             return localized
         }
 
-        return localizationBundle.localizedString(forKey: self, value: self, table: nil)
+        var localized = NSLocalizedString(self, tableName: "InitialString", bundle: localizationBundle, value: self, comment: "")
+         if localized == self {
+           localized = localizationBundle.localizedString(forKey: self, value: self, table: nil)
+         }
+         return localized
     }
 }
